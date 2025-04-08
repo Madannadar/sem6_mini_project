fe59aba81375a5cd76763414e27679c6f6c84b3f
Madannadar <madanak0509@gmail.com>
2025-04-08 20:44:03 +0530

    fix: update CORS configuration to allow all origins and clean up requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -15,9 +15,7 @@ MODEL_PATH = os.path.join(BASE_DIR, "container", "weights", "best.pt")
 
 # Initialize Flask app and model
 app = Flask(__name__)
-CORS(app, origins=["https://sem6-mini-project.vercel.app"])
-
-
+CORS(app)
 model = YOLO(MODEL_PATH)
 
 @app.route('/detect', methods=['POST'])
